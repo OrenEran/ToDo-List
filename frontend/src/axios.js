4a45d74954b16ac9a61dda667561623136054fa1
Eran Oren <eran.oren@gmail.com>
2025-01-26 20:45:54 +0200

    axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: 'http://localhost:8000/api' || process.env.REACT_APP_API_BASE_URL ,
   headers: {
     'Content-Type': 'application/json'
   }
